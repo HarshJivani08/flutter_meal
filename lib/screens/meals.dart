596a5725bba5f6da77d86c71bcda908466395abf
HarshJivani08 <harshjivani140901@gmail.com>
2024-05-10 18:07:05 +0530

    added meal detail screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -8,6 +8,11 @@ class MealsScreen extends StatelessWidget {
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => MealDetails(meal: meal),));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +22,30 @@ class MealsScreen extends StatelessWidget {
         children: [
           Text(
             'Uh oh ... nothing here!',
-            style: Theme.of(context)
+            style: Theme
+                .of(context)
                 .textTheme
                 .headlineLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+                .copyWith(color: Theme
+                .of(context)
+                .colorScheme
+                .onBackground),
           ),
           const SizedBox(
             height: 16,
           ),
           Text(
             'Try selecting a different category!',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
+            style: Theme
+                .of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(
+              color: Theme
+                  .of(context)
+                  .colorScheme
+                  .onBackground,
+            ),
           ),
         ],
       ),
@@ -38,10 +54,13 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
-            // style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-            //   color: Theme.of(context).colorScheme.onBackground,
-            // )
+        itemBuilder: (ctx, index) =>
+            MealItem(meal: meals[index], onSelectMeal:(meal){
+               selectMeal(context, meal);
+            },),
+        // style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+        //   color: Theme.of(context).colorScheme.onBackground,
+        // )
 
       );
     }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8),),
-      clipBehavior:Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -23,7 +39,7 @@ class MealItem extends StatelessWidget {
               image: NetworkImage(meal.imageUrl),
               fit: BoxFit.cover,
               height: 200,
-
+              width: double.infinity,
             ),
             Positioned(
               bottom: 0,
@@ -31,7 +47,8 @@ class MealItem extends StatelessWidget {
               right: 0,
               child: Container(
                 color: Colors.black54,
-                padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
                 child: Column(
                   children: [
                     Text(
@@ -43,12 +60,21 @@ class MealItem extends StatelessWidget {
                       style: const TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white
-                      ),
+                          color: Colors.white),
                     ),
                     const SizedBox(height: 12),
-                    const Row(
-                      children: [],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} min'),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                            icon: Icons.attach_money, label: affordabilityText),
+                      ],
                     )
                   ],
                 ),
